7fd6c330d25bcd3703d24bad6201bac22b760b32
shum01 <2021154167@ub.edu.bz>
2026-02-03 20:57:35 -0600

    part2

---
+++ exercise2.js
@@ -0,0 +1,12 @@
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squared = numbers.map(num => num * num);
+
+const evens = numbers.filter(num => num % 2 === 0);
+
+const sum = numbers.reduce((total, num) => total + num, 0);
+
+//outputs
+console.log("Even numbers:", evens);  
+console.log("Sum:", sum);             
