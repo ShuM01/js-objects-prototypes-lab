bceea50334b5b8c967c3f73e6c76aba2224c5ce4
shum01 <2021154167@ub.edu.bz>
2026-02-03 20:59:26 -0600

    part2

--- exercise2.js
+++ exercise2.js
@@ -7,7 +7,7 @@ const evens = numbers.filter(num => num % 2 === 0);
 
 const sum = numbers.reduce((total, num) => total + num, 0);
 
-//result
+//test result
 console.log("Squared:", squared); 
 console.log("Even numbers:", evens);    
 console.log("Sum:", sum);             
